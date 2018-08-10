f16450e379f2d5954b8f24bfffdf5aac94d6f220
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-08-09 23:11:24 -0700

    can teleport.

--- src/headset/Assets/Scripts/User/Movement.cs
+++ src/headset/Assets/Scripts/User/Movement.cs
@@ -5,93 +5,64 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
- //   private const string TELEPORT_CAPABLE_TAG = "TeleportReceiver";
+    private const string TELEPORT_CAPABLE_TAG = "TeleportReceiver";
+    private const string CONTROLLER_TAG = "GameController";
 
- //   private bool wasStickHeldLastFrame;
- //   private Vector3 currentTargetPosition;
- //   private Vector2 currentStickPosition;
- //   private NetworkManager networkManager;
+    private bool wasStickHeldLastFrame;
+    private Vector3 currentTargetPosition;
+    private Vector2 currentStickPosition;
+    private NetworkManager networkManager;
 
- //   public float TeleportThreshold = 0.5f;
- //   public float NoHoldThreshold = 0.5f;
- //   public float RotateThreshold = 0.4f;
- //   public float RotateExponent = 1.5f;
- //   public float MaxRotateSpeed = 1.25f;
+    public float TeleportThreshold = 0.5f;
+    public float NoHoldThreshold = 0.5f;
+    public float RotateThreshold = 0.4f;
+    public float RotateExponent = 1.5f;
+    public float MaxRotateSpeed = 1.25f;
 
- //   public GameObject NetworkManagerObject;
+    public GvrLaserPointer GvrLaserPointer;
 
-	//// Use this for initialization
-	//void Start () {
- //       networkManager = NetworkManagerObject.GetComponent<NetworkManager>();
-	//}
+    public GameObject NetworkManagerObject;
 
- //   // Update is called once per frame
- //   void Update () {
- //       OVRInput.Update();
+    // Use this for initialization
+    void Start()
+    {
+        networkManager = NetworkManagerObject.GetComponent<NetworkManager>();
+    }
 
- //       var secondaryThumbstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+    // Update is called once per frame
+    void Update()
+    {
+        GvrPointerInputModule.Pointer = GvrLaserPointer;
 
- //       Teleport(secondaryThumbstick);
- //       Rotate(secondaryThumbstick);
- //   }
+        var device = GvrControllerInput.GetDevice(GvrControllerHand.Dominant);
 
- //   private void Teleport(Vector2 thumbstick)
- //   {
- //       var lineRender = GetComponent<LineRenderer>();
- //       if (thumbstick.y > TeleportThreshold)
- //       {
- //           lineRender.enabled = true;
+        //var secondaryThumbstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
 
- //           var rTouchLocalPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
- //           var rTouchWorldPosition = transform.TransformPoint(rTouchLocalPosition);
+        Teleport(device);
+        //Rotate(secondaryThumbstick);
+    }
 
- //           var rTouchLocalForward = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch) * Vector3.forward;
- //           var rTouchWorldForward = transform.TransformDirection(rTouchLocalForward);
+    private void Teleport(GvrControllerInputDevice device)
+    {
+        if (device.GetButtonUp(GvrControllerButton.TouchPadButton) &&
+            GvrLaserPointer.CurrentRaycastResult.gameObject != null &&
+            GvrLaserPointer.CurrentRaycastResult.gameObject.tag == TELEPORT_CAPABLE_TAG)
+        {
 
- //           var ray = new Ray(rTouchWorldPosition, rTouchWorldForward);
- //           RaycastHit hit = default(RaycastHit);
- //           if (GameObject.FindGameObjectsWithTag(TELEPORT_CAPABLE_TAG).Any(x => x.GetComponent<Collider>().Raycast(ray, out hit, 100.0f)))
- //           {
- //               currentTargetPosition = hit.point;
- //               currentStickPosition = thumbstick;
+            transform.position = GvrLaserPointer.CurrentRaycastResult.worldPosition + new Vector3(0, 1.5f, 0);
+        }
+    }
 
- //               lineRender.SetPositions(new Vector3[]
- //               {
- //                  rTouchWorldPosition,
- //                  currentTargetPosition
- //               });
- //           }
- //           else
- //           {
- //               lineRender.enabled = false;
- //           }
+    private void Rotate(Vector2 thumbstick)
+    {
+        if (Mathf.Abs(thumbstick.x) > RotateThreshold)
+        {
+            var eulerAngles = transform.rotation.eulerAngles;
+            var direction = Mathf.Abs(thumbstick.x) / thumbstick.x;
+            eulerAngles.y += direction * Mathf.Min(Mathf.Pow(RotateExponent, Mathf.Abs(thumbstick.x)), MaxRotateSpeed);
+            transform.eulerAngles = eulerAngles;
 
- //           wasStickHeldLastFrame = true;
- //       }
- //       else
- //       {
- //           lineRender.enabled = false;
-
- //           if (wasStickHeldLastFrame && thumbstick.magnitude < NoHoldThreshold)
- //           {
- //               transform.position = currentTargetPosition + new Vector3(0, 1.5f, 0);
- //               networkManager.RaiseEvent(NetworkManager.EventCode.PlayerPositionChanged, transform.position);
- //           }
-
- //           wasStickHeldLastFrame = false;
- //       }
- //   }
-
- //   private void Rotate(Vector2 thumbstick)
- //   {
- //       if (Mathf.Abs(thumbstick.x) > RotateThreshold)
- //       {
- //           var eulerAngles = transform.rotation.eulerAngles;
- //           var direction = Mathf.Abs(thumbstick.x) / thumbstick.x;
- //           eulerAngles.y += direction * Mathf.Min(Mathf.Pow(RotateExponent, Mathf.Abs(thumbstick.x)), MaxRotateSpeed);
- //           transform.eulerAngles = eulerAngles;
-
- //           networkManager.RaiseEvent(NetworkManager.EventCode.PlayerRotationChanged, eulerAngles.y);
- //       }
- //   }
+            networkManager.RaiseEvent(NetworkManager.EventCode.PlayerRotationChanged, eulerAngles.y);
+        }
+    }
 }
